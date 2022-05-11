31a1249fc656ab667adef80ff1e6db2dc59f3f2a
Rsmile310 <78564494+Rsmile310@users.noreply.github.com>
2022-05-11 21:52:44 +0900

    update

--- src/pages/CameraPage/index.jsx
+++ src/pages/CameraPage/index.jsx
@@ -45,18 +45,18 @@ const CameraPage = () => {
     alert("handle preview")
   }, [recordedChunks]);
 
-  const handleDownload = () => {
-    alert("Downloading now");
-    // const a = document.createElement("a");
-    // document.body.appendChild(a);
-    // a.style = "display: none";
-    // a.href = url;
-    // a.download = "react-webcam-stream-capture.webm";
-    // a.click();
-    // window.URL.revokeObjectURL(url);
-    // setRecordedChunks([]);
-    // }
-  };
+  // const handleDownload = () => {
+  //   alert("Downloading now");
+  //   // const a = document.createElement("a");
+  //   // document.body.appendChild(a);
+  //   // a.style = "display: none";
+  //   // a.href = url;
+  //   // a.download = "react-webcam-stream-capture.webm";
+  //   // a.click();
+  //   // window.URL.revokeObjectURL(url);
+  //   // setRecordedChunks([]);
+  //   // }
+  // };
 
   return (
     <div className="cameraPage mainPage">
