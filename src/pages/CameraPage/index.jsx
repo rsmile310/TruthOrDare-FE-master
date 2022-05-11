8166b69efdc9a0cd0901b872001b17c080db91d3
Rsmile310 <78564494+Rsmile310@users.noreply.github.com>
2022-05-11 23:38:07 +0900

    update

--- src/pages/CameraPage/index.jsx
+++ src/pages/CameraPage/index.jsx
@@ -7,9 +7,9 @@ import Webcam from "react-webcam";
 const CameraPage = () => {
   const webcamRef = React.useRef(null);
   const mediaRecorderRef = React.useRef(null);
-  const [capturing, setCapturing] = React.useState(true);
+  // const [capturing, setCapturing] = React.useState(false);
   const [recordedChunks, setRecordedChunks] = React.useState([]);
-  // const [videoUrl, setVideoUrl] = React.useState("");
+  const [videoUrl, setVideoUrl] = React.useState("");
 
   const handleStartCaptureClick = React.useCallback(() => {
     mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
@@ -33,15 +33,7 @@ const CameraPage = () => {
 
   const handleStopCaptureClick = React.useCallback(() => {
     mediaRecorderRef.current.stop();
-    setCapturing(false);
-    const blob = new Blob(recordedChunks, {
-      type: "video/webm",
-    });
-    const url = window.URL.createObjectURL(blob);
-    // setVideoUrl(url);
-    const videoPreview = document.querySelector(".previewVideo");
-    videoPreview.src = url;
-    videoPreview.load();
+    handlePreview();
   }, [mediaRecorderRef, webcamRef]);
 
   const handlePreview = React.useCallback(() => {
@@ -49,25 +41,21 @@ const CameraPage = () => {
       type: "video/webm",
     });
     const url = window.URL.createObjectURL(blob);
-    // setVideoUrl(url);
-    const videoPreview = document.querySelector(".previewVideo");
-    videoPreview.src = url;
-    videoPreview.load();
+    setVideoUrl(url);
   }, [recordedChunks]);
 
-  // const handleDownload = () => {
-  //   alert("Downloading now");
-  //   // const a = document.createElement("a");
-  //   // document.body.appendChild(a);
-  //   // a.style = "display: none";
-  //   // a.href = url;
-  //   // a.download = "react-webcam-stream-capture.webm";
-  //   // a.click();
-  //   // window.URL.revokeObjectURL(url);
-  //   // setRecordedChunks([]);
-  //   // }
-  // };
-
+  const handleDownload = () => {
+    alert("Downloading now");
+    // const a = document.createElement("a");
+    // document.body.appendChild(a);
+    // a.style = "display: none";
+    // a.href = url;
+    // a.download = "react-webcam-stream-capture.webm";
+    // a.click();
+    // window.URL.revokeObjectURL(url);
+    // setRecordedChunks([]);
+    // }
+  };
   return (
     <div className="cameraPage mainPage">
       <button className="cameraCloseBtn iconBtn">
@@ -90,18 +78,18 @@ const CameraPage = () => {
       >
         {/* <img src="/images/other/Camera_Button.png" alt="" /> */}
       </button>
-      {!capturing && (
+      {recordedChunks.length > 0 && (
         <div className="videoContainer">
           <div className="videoBox">
             <video
-              className="previewVideo"
+              src={videoUrl}
               width="302px"
               height="390px"
               controls
               title="video"
-              onClick={handlePreview}
             />
-            <button className="iconBtn downloadBtn" onClick={handlePreview}>
+            <div class="bt-play"></div>
+            <button className="iconBtn downloadBtn" onClick={handleDownload}>
               <img
                 src="/images/other/download.png"
                 width="50px"
@@ -113,20 +101,25 @@ const CameraPage = () => {
         </div>
       )}
       {/* <div className="videoContainer">
-          <div className="videoBox">
-            <video width='302' height='390' controls autoplay>
-                <source src={videoUrl} type="video/webm" />
-            </video>
-            <button className="iconBtn downloadBtn" onClick={handlePreview}>
-              <img
-                src="/images/other/download.png"
-                width="50px"
-                height="50px"
-                alt=""
-              />
-            </button>
-          </div>
-        </div> */}
+        <div className="videoBox">
+          <video
+            src={videoUrl}
+            width="302px"
+            height="390px"
+            controls
+            title="video"
+          />
+          <div class="bt-play" onClick={handleDownload}></div>
+          <button className="iconBtn downloadBtn" onClick={handlePreview}>
+            <img
+              src="/images/other/download.png"
+              width="50px"
+              height="50px"
+              alt=""
+            />
+          </button>
+        </div>
+      </div> */}
     </div>
   );
 };
