3e984a2bcc450aebb74f09f7bcf4c59b686bdf0e
Rsmile310 <78564494+Rsmile310@users.noreply.github.com>
2022-05-11 19:31:27 +0900

    udpate8

--- src/pages/CameraPage/index.jsx
+++ src/pages/CameraPage/index.jsx
@@ -40,7 +40,7 @@ const CameraPage = () => {
       const url = window.URL.createObjectURL(blob);
       setVideoUrl(url);
       // window.URL.revokeObjectURL(url);
-      alert("recordedChunks");
+      alert(recordedChunks);
     // }
     alert("stop");
   }, [mediaRecorderRef, webcamRef]);
