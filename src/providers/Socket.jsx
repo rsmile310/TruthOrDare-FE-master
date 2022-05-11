d3aba770f7c0aae785f80365d1b8c6951ce8dcc1
Rsmile310 <78564494+Rsmile310@users.noreply.github.com>
2022-05-11 14:39:18 +0900

    update3

--- src/providers/Socket.jsx
+++ src/providers/Socket.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { SERVER_URL } from "../const";
 import socketIOClient from "socket.io-client";
